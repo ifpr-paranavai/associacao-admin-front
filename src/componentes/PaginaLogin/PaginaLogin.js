706e29176ec2095b5d803ab61662f3568751f54c
Raquel Santos <raquel.dossantos1965@gmail.com>
2021-04-14 16:22:53 -0300

    Criação da página de login

--- src/componentes/PaginaLogin/PaginaLogin.js
+++ src/componentes/PaginaLogin/PaginaLogin.js
@@ -1,40 +1,68 @@
 import React from 'react';
-import {Grid, TextField, Button, Paper} from '@material-ui/core';
+import clsx from 'clsx';
+import { useStyles } from './estilo.js';
+import {AccountCircle, Visibility, VisibilityOff} from '@material-ui/icons';
+import {Grid, Paper, Avatar, TextField, OutlinedInput, InputAdornment, IconButton, InputLabel, Button, FormControl} from '@material-ui/core';
 
 export default function PaginaLogin() {
-  return (
-      <Grid container justify="center" styles={{ minHeight : '100vh'}}>
-            <Paper elevation={3}>
-                <Grid 
-                item 
-                xs={12} 
-                sm ={6} 
-                alignItems="center" 
-                justify="center"
-                direction="column" 
-                style={{padding: 100}}>
-                  <div  />
-                  <div style={{display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth:300}}> 
 
-                    <Grid container justify="center">
-                      <img 
-                      src="https://th.bing.com/th/id/R331f336a7b480df03c074f32ecc8d6c2?rik=ajdKHhD8EDu%2fZw&pid=ImgRaw"
-                      widht={100}
-                      height={100}
-                      alt="logo"
-                      />              
-                    </Grid>
-                    <TextField label="Email" margin="normal" variant="outlined"/>
-                    <TextField label="Senha" margin="normal" variant="outlined" type="password"/>
-                    <div style={{ height: 20}} />
-                  <Button color="primary" variant="contained">
-                    Entrar
-                  </Button>           
-                  </div>
-                </Grid> 
-            </Paper>    
+  const classes = useStyles();
+
+  const [values, setValues] = React.useState({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return(
+    <Grid className={classes.image.src}>
+      <Paper elevation={10} className={classes.paper}> 
+        <Grid align="center">
+          <Avatar className={classes.avatar}>
+            <AccountCircle fontSize="large"/>
+          </Avatar>
+          <h2>Login</h2>
         </Grid>
-      
+        <TextField label="E-mail" variant="outlined" fullWidth/>
+        <div style={{ height: 20}} />
+        <FormControl className={clsx(classes.margin)} variant="outlined" fullWidth>
+          <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
+            />
+        </FormControl>
+        <div style={{ height: 40}} />
+        <Button className={classes.button_submit} type="submit" color="primary" variant="contained" fullWidth>
+          Entrar
+        </Button> 
+      </Paper>
+    </Grid>
   );
-    
 }
