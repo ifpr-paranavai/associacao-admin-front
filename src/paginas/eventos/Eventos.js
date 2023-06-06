e4576ffc2c9818eeec003f3cfe91fe5ec16ee519
JoileJr <joilejunior2@gmail.com>
2023-06-06 10:40:45 -0300

    listando eventos

--- src/paginas/eventos/Eventos.js
+++ src/paginas/eventos/Eventos.js
@@ -1,8 +1,52 @@
-import React, { useEffect } from 'react';
-import { useNavigation } from '../../contextos/Navegacao';
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Paper,
+  Avatar,
+  IconButton,
+  Container,
+  TableContainer,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  TablePagination,
+  Button,
+  LinearProgress,
+  CircularProgress,
+  colors,
+  InputAdornment,
+} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import InputMask from 'react-input-mask';
+
+import {
+  Edit as EditIcon,
+  Add as AddIcon,
+  Delete as DeleteIcon,
+  Search as SearchIcon,
+} from '@material-ui/icons';
+import { FaWhatsapp } from 'react-icons/fa';
+
+import { useDebouncedCallback } from 'use-debounce';
+import ServicoEvento from '../../servicos/ServicoEvento';
 import Breadcrumbs from '../../componentes/Breadcrumbs/Breadcrumbs';
 
+import styles from './estilo.css';
+import { useNotify } from '../../contextos/Notificacao';
+import { useNavigation } from '../../contextos/Navegacao';
+import { formatarData } from '../../uteis/formatarData';
+
 function Eventos() {
+  const [dados, setDados] = useState([]);
+
   const { setLocation } = useNavigation();
   useEffect(() => {
     setLocation({
@@ -12,7 +56,82 @@ function Eventos() {
     });
   }, []);
 
-  return <Breadcrumbs />;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const dadosAPI = await ServicoEvento.listarEventos();
+        setDados(dadosAPI);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <Container className={styles.root}>
+      <Breadcrumbs />
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        paddingBottom="18px"
+        paddingTop="12px"
+      >
+        <TextField
+          placeholder="Buscar por titulo ou data"
+          variant="outlined"
+          size="small"
+          style={{ width: '100%', maxWidth: '400px' }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon color="action" />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" color="primary" startIcon={<AddIcon />}>
+          Adicionar
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table className={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Titulo</TableCell>
+              <TableCell>Descrição</TableCell>
+              <TableCell>Link</TableCell>
+              <TableCell>Data</TableCell>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dados.map(item => (
+              <TableRow key={item.id}>
+                <TableCell>{item.titulo}</TableCell>
+                <TableCell>{item.descricao}</TableCell>
+                <TableCell>{item.link}</TableCell>
+                <TableCell>
+                  {formatarData(item.data_inicio)} - {formatarData(item.data_fim)}
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton aria-label="editar">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="deletar">
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
 export default Eventos;
---
+++ src/servicos/ServicoEvento.js
@@ -0,0 +1,35 @@
+import Axios from 'axios';
+import Config from '../uteis/configuracao';
+
+class ServicoEvento {
+  static async listarEventos() {
+    try {
+      const response = await Axios.get(`${Config.api}/eventos`);
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao obter dados da API:', error);
+      throw error;
+    }
+  }
+
+  static async cadastrarEvento(evento) {
+    const { data } = await Axios.post(`${Config.api}/eventos`, evento);
+    return data;
+  }
+
+  static async atualizarEvento(evento, id) {
+    const { data } = await Axios.put(`${Config.api}/eventos/${id}`, evento);
+    return data;
+  }
+
+  static async buscarPorId(id) {
+    const { data } = await Axios.get(`${Config.api}/eventos/${id}`);
+    return data;
+  }
+
+  static async deletarEvento(id) {
+    await Axios.delete(`${Config.api}/eventos/${id}`);
+  }
+}
+
+export default ServicoEvento;
