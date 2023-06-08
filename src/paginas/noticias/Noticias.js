ba2633288d87cef0499428729e7ecd4660c51ed2
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-06-08 17:08:48 -0300

    Add/Pagina Noticias

--- src/paginas/noticias/Noticias.js
+++ src/paginas/noticias/Noticias.js
@@ -1,18 +1,225 @@
-import React, { useEffect } from 'react';
-import { useNavigation } from '../../contextos/Navegacao';
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Paper,
+  Avatar,
+  IconButton,
+  Container,
+  TableContainer,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  TablePagination,
+  Button,
+  LinearProgress,
+  CircularProgress,
+  colors,
+  InputAdornment,
+} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import InputMask from 'react-input-mask';
+
+import {
+  Edit as EditIcon,
+  Add as AddIcon,
+  Delete as DeleteIcon,
+  Search as SearchIcon,
+} from '@material-ui/icons';
+import { FaWhatsapp } from 'react-icons/fa';
+
+import { useDebouncedCallback } from 'use-debounce';
+import CadastrarNoticia from '../../componentes/CadastrarNoticia/CadastrarNoticia';
+import ServicoNoticia from '../../servicos/ServicoNoticia';
 import Breadcrumbs from '../../componentes/Breadcrumbs/Breadcrumbs';
 
+import styles from './estilo.css';
+import { useNotify } from '../../contextos/Notificacao';
+import { useNavigation } from '../../contextos/Navegacao';
+import { formatarData } from '../../uteis/formatarData';
+
 function Noticias() {
+  const [noticias, setNoticias] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [noticiaSelecionado, setNoticiaSelecionado] = useState(null);
+  const [deleteDialog, setDeleteDialog] = useState(false);
+  const [removing, setRemoving] = useState(false);
+  const notify = useNotify();
+
+  const abrirFormulario = () => {
+    setOpen(true);
+  };
+  const fecharFormulario = () => {
+    setOpen(false);
+  };
+
+  function onSaveNoticia() {
+    fecharFormulario();
+  }
+
+  function onCloseRemoveNoticia() {
+    setDeleteDialog(false);
+    setNoticiaSelecionado(null);
+  }
+
+  async function handleRemoveNoticia() {
+    try {
+      setRemoving(true);
+      await ServicoNoticia.deletarNoticia(noticiaSelecionado.id);
+      onCloseRemoveNoticia();
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } catch (error) {
+      notify.showError(error.message);
+    } finally {
+      setRemoving(false);
+    }
+  }
+
   const { setLocation } = useNavigation();
   useEffect(() => {
     setLocation({
-      title: 'Gestão de Notícias',
+      title: 'Gestão de Noticias',
       key: 'noticias',
       path: '/noticias',
     });
   }, []);
 
-  return <Breadcrumbs />;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const dadosAPI = await ServicoNoticia.listarNoticias();
+        setNoticias(dadosAPI);
+      } catch (error) {
+        // console.error('Erro ao buscar dados da API:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <Container className={styles.root}>
+      <Breadcrumbs />
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        paddingBottom="18px"
+        paddingTop="12px"
+      >
+        <TextField
+          placeholder="Buscar por titulo ou data"
+          variant="outlined"
+          size="small"
+          style={{ width: '100%', maxWidth: '400px' }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon color="action" />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+          onClick={abrirFormulario}
+        >
+          Adicionar
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table className={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Titulo</TableCell>
+              <TableCell>Descrição</TableCell>
+              <TableCell>Data</TableCell>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {noticias.map(noticia => (
+              <TableRow key={noticia.id}>
+                <TableCell className={styles.celula}>{noticia.titulo}</TableCell>
+                <TableCell className={styles.celula}>{noticia.descricao}</TableCell>
+                <TableCell className={styles.celula}>
+                  {formatarData(noticia.data_inicio)}
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    aria-label="editar"
+                    onClick={() => {
+                      setNoticiaSelecionado(noticia);
+                      setOpen(true);
+                    }}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label="deletar"
+                    onClick={() => {
+                      setNoticiaSelecionado(noticia);
+                      setDeleteDialog(true);
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <CadastrarNoticia
+        open={open}
+        noticia={noticiaSelecionado}
+        fecharFormulario={fecharFormulario}
+        onSave={() => onSaveNoticia()}
+      />
+      <Dialog
+        open={deleteDialog}
+        onClose={() => onCloseRemoveNoticia()}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title" style={{ padding: '20px' }}>
+          Excluir associado:
+          {noticiaSelecionado && (
+            <span style={{ marginRight: '10px', marginLeft: '10px' }}>
+              {noticiaSelecionado.titulo}
+            </span>
+          )}
+        </DialogTitle>
+        <DialogActions style={{ justifyContent: 'space-around', padding: '10px' }}>
+          <Button color="primary" onClick={() => onCloseRemoveNoticia()}>
+            Cancelar
+          </Button>
+          <div className={styles.wrapper}>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={removing}
+              onClick={() => handleRemoveNoticia()}
+            >
+              Excluir
+            </Button>
+            {removing && <CircularProgress size={24} className={styles.buttonProgress} />}
+          </div>
+        </DialogActions>
+      </Dialog>
+    </Container>
+  );
 }
 
 export default Noticias;
