dcc0eeeeb8d3e3fe914bbdfd0c664e48e0b66bf8
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-06-20 15:21:00 -0300

    Mudança de Table para Card

--- src/paginas/eventos/Eventos.js
+++ src/paginas/eventos/Eventos.js
@@ -3,6 +3,12 @@ import {
   Box,
   Paper,
   Checkbox,
+  Card,
+  Grid,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Link,
   Avatar,
   IconButton,
   Container,
@@ -200,59 +206,71 @@ function Eventos() {
           </Button>
         </Box>
       </Box>
-      <TableContainer component={Paper}>
-        <Table className={styles.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell />
-              <TableCell>Titulo</TableCell>
-              <TableCell>Imagem</TableCell>
-              <TableCell>Descrição</TableCell>
-              <TableCell>Local</TableCell>
-              <TableCell>Data</TableCell>
-              <TableCell />
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {eventos.map(evento => (
-              <TableRow key={evento.id}>
-                <TableCell padding="checkbox">
-                  <Checkbox onChange={event => handleSelectEvento(event, evento.id)} />
-                </TableCell>
-                <TableCell className={styles.celula}>{evento.titulo}</TableCell>
-                <TableCell>
-                  <img src={evento.url} alt="Preview" width="100" />
-                </TableCell>
-                <TableCell className={styles.celula}>{evento.descricao}</TableCell>
-                <TableCell className={styles.celula}>{evento.local}</TableCell>
-                <TableCell className={styles.celula}>
-                  {formatarData(evento.data_inicio)} - {formatarData(evento.data_fim)}
-                </TableCell>
-                <TableCell align="right">
-                  <IconButton
-                    aria-label="editar"
-                    onClick={() => {
-                      setEventoSelecionado(evento);
-                      setOpen(true);
-                    }}
-                  >
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton
-                    aria-label="deletar"
-                    onClick={() => {
-                      setEventoSelecionado(evento);
-                      setDeleteDialog(true);
-                    }}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Grid container spacing={3}>
+        {eventos.map(evento => (
+          <Grid item key={evento.id} xs={12} sm={6} md={4}>
+            <Card style={{ borderRadius: '16px' }}>
+              <CardMedia
+                component="img"
+                alt="Imagem do Evento"
+                height="220"
+                image={evento.url}
+                title="Imagem do Evento"
+              />
+              <CardContent>
+                <h2 style={{ fontFamily: 'Arial', wordWrap: 'break-word' }}>
+                  Título: {evento.titulo}
+                </h2>
+                <p
+                  style={{
+                    fontFamily: 'Arial',
+                    fontSize: 18,
+                    wordWrap: 'break-word',
+                  }}
+                >
+                  Descrição: {evento.descricao}
+                </p>
+                <p style={{ fontFamily: 'Arial', fontSize: 16, wordWrap: 'break-word' }}>
+                  Local: {evento.local}
+                </p>
+                <p style={{ fontFamily: 'Arial', fontSize: 16, wordWrap: 'break-word' }}>
+                  Data:{' '}
+                  {`${formatarData(evento.data_inicio)} a ${formatarData(
+                    evento.data_fim,
+                  )}`}
+                </p>
+                <Link href={evento.link} target="_blank" rel="noopener">
+                  {evento.link}
+                </Link>
+              </CardContent>
+              <CardActions>
+                <Checkbox
+                  onChange={event => handleSelectEvento(event, evento.id)}
+                  checked={selectedEventos.includes(evento.id)}
+                />
+                <IconButton
+                  aria-label="editar"
+                  onClick={() => {
+                    setEventoSelecionado(evento);
+                    setOpen(true);
+                  }}
+                >
+                  <EditIcon />
+                </IconButton>
+                <IconButton
+                  aria-label="deletar"
+                  onClick={() => {
+                    setEventoSelecionado(evento);
+                    setDeleteDialog(true);
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
       <CadastrarEvento
         open={open}
         evento={eventoSelecionado}
